181b6c7af8f57e2fc45b3df40445369b43570b95
Anne Zheng <anne.zheng.dev@gmail.com>
2018-04-28 15:00:52 -0700

    Added endpoint for getting sorted task list by using 'rank' algorithm.

--- app/src/main/java/com/walmart/labs/controller/TaskController.java
+++ app/src/main/java/com/walmart/labs/controller/TaskController.java
@@ -5,6 +5,7 @@ import com.walmart.labs.dto.ResponseDTO;
 import com.walmart.labs.dto.TaskDTO;
 import com.walmart.labs.dto.TaskTemplateDTO;
 import com.walmart.labs.service.TaskService;
+import java.util.Arrays;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -33,7 +34,7 @@ public class TaskController {
    * @return List of TaskDTO that the user is authorized to query.
    */
   @RequestMapping(value = "/task", method = RequestMethod.GET)
-  public ResponseEntity<ResponseDTO<List<TaskDTO>>> getTaskListByUser() {
+  public ResponseEntity<ResponseDTO<List<TaskDTO>>> getTaskList() {
     ResponseDTO<List<TaskDTO>> responseDTO = new ResponseDTO<>();
 
     OAuth2Authentication authentication =
@@ -44,6 +45,20 @@ public class TaskController {
     responseDTO.addMessage("Get task list by user successfully!");
     responseDTO.setSuccess(true);
     responseDTO.setResult(taskDTOList);
+    return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+  }
+
+  @RequestMapping(value = "/task/sort/rank", method = RequestMethod.GET)
+  public ResponseEntity<ResponseDTO<List<TaskDTO>>> getTaskListSortByRank() {
+    ResponseEntity<ResponseDTO<List<TaskDTO>>> responseEntity = getTaskList();
+    ResponseDTO<List<TaskDTO>> responseDTO = responseEntity.getBody();
+    if (!responseDTO.getSuccess() || responseDTO.getResult().size() <= 1) {
+      return responseEntity;
+    }
+    List<TaskDTO> taskDTOListSortedById = responseDTO.getResult();
+    List<TaskDTO> taskDTOListSortedByRank = taskService.sortTaskDTOListByRank(taskDTOListSortedById);
+    responseDTO.setResult(taskDTOListSortedByRank);
+    responseDTO.addMessage("Get task list sorted by rank");
     return new ResponseEntity<>(responseDTO, HttpStatus.OK);
   }
 
@@ -81,6 +96,10 @@ public class TaskController {
   /**
    * Update a existing task and save back to database. Needs dto object in the request body.
    *
+   * <p>Request URL: /task
+   *
+   * <p>Request Method: PUT
+   *
    * @param taskDTO request body {@link TaskDTO} contains task name, task status, task description,
    *     task note, task feedback, the time when user click create task button (timeInput),
    *     estimated finish time, isRecurring indicates if this is a recurring tasks,
@@ -108,6 +127,10 @@ public class TaskController {
   /**
    * Delete a existing task by provided task id.
    *
+   * <p>Request URL: /task
+   *
+   * <p>Request Method: DELETE
+   *
    * @param taskId taskId should be an id for an existing task record in database.
    * @return Empty result with success/fail messages.
    */
@@ -125,8 +148,19 @@ public class TaskController {
     return new ResponseEntity<>(responseDTO, HttpStatus.OK);
   }
 
+  /**
+   * Get task template from database based on user's role. This endpoint does not need any inputs
+   * (only the access_token). Backend will get the user information from the access_token and query
+   * all the tasks that the user is authorized to query.
+   *
+   * <p>Request URL: /task/template
+   *
+   * <p>Request Method: GET
+   *
+   * @return List of TaskTemplate that the user is authorized to check
+   */
   @RequestMapping(value = "/task/template", method = RequestMethod.GET)
-  public ResponseEntity<ResponseDTO<List<TaskTemplateDTO>>> getTaskTemplateListByUser() {
+  public ResponseEntity<ResponseDTO<List<TaskTemplateDTO>>> getTaskTemplateList() {
     ResponseDTO<List<TaskTemplateDTO>> responseDTO = new ResponseDTO<>();
 
     OAuth2Authentication authentication =
@@ -140,6 +174,21 @@ public class TaskController {
     return new ResponseEntity<>(responseDTO, HttpStatus.OK);
   }
 
+  /**
+   * Create task template based on TaskDTO! This endpoint is mainly used for frontend to create task
+   * template in the task creation page. This endpoint needs user fill out all the information for
+   * creating a 'Task'. the service will extract information from taskDTO and save them as
+   * TaskTemplate
+   *
+   * <p>Request URL: /task/template
+   *
+   * <p>Request Method: POST
+   *
+   * @param taskDTO taskDTO contains redundant information such as TackStatus, feedback. But
+   *     contains every fields in TaskTemplate. So we can reuse this to receive request from
+   *     frontend.
+   * @return Empty result with success/fail messages.
+   */
   @RequestMapping(value = "/task/template", method = RequestMethod.POST)
   public ResponseEntity<ResponseDTO<Void>> createTaskTemplate(@RequestBody TaskDTO taskDTO) {
     ResponseDTO<Void> responseDTO = new ResponseDTO<>();
@@ -154,6 +203,18 @@ public class TaskController {
     return new ResponseEntity<>(responseDTO, HttpStatus.OK);
   }
 
+  /**
+   * Update task template by taskTemplateDTO.
+   *
+   * <p>Request URL: /task/template
+   *
+   * <p>Request Method: PUT
+   *
+   * @param taskTemplateDTO {@link TaskDTO} contains similar information. taskTemplateId is required
+   *     field. Other fields are optional, Fields that presented will be updated and saved in
+   *     database.
+   * @return Empty result with success/fail messages.
+   */
   @RequestMapping(value = "/task/template", method = RequestMethod.PUT)
   public ResponseEntity<ResponseDTO<Void>> updateTaskTemplate(
       @RequestBody TaskTemplateDTO taskTemplateDTO) {
@@ -169,6 +230,12 @@ public class TaskController {
     return new ResponseEntity<>(responseDTO, HttpStatus.OK);
   }
 
+  /**
+   * Delete existing task template from database according to provided taskTemplateId
+   *
+   * @param taskTemplateId delete the records in database with taskTemplateId
+   * @return Empty result with success/fail messages.
+   */
   @RequestMapping(value = "/task/template", method = RequestMethod.DELETE)
   public ResponseEntity<ResponseDTO<Void>> updateTask(@RequestParam Long taskTemplateId) {
     ResponseDTO<Void> responseDTO = new ResponseDTO<>();
--- app/src/main/java/com/walmart/labs/domain/TaskPriority.java
+++ app/src/main/java/com/walmart/labs/domain/TaskPriority.java
@@ -22,4 +22,22 @@ public enum TaskPriority {
     }
     return null;
   }
+
+  public static int compareTwoPriorities(String p1String, String p2String) {
+    if (p1String.equals(p2String)) {
+      return 0;
+    } else {
+      TaskPriority p1 = TaskPriority.valueOf(p1String);
+      TaskPriority p2 = TaskPriority.valueOf(p2String);
+      if (p1.equals(TaskPriority.HIGH)) {
+        return -1;
+      } else if (p1.equals(TaskPriority.MEDIUM) && p2.equals(TaskPriority.LOW)) {
+        return -1;
+      } else if (p1.equals(TaskPriority.MEDIUM) && p2.equals(TaskPriority.HIGH)) {
+        return 1;
+      } else {
+        return 1;
+      }
+    }
+  }
 }
